3fa83fcd9e87645869254c041b518d1fa5593ca8
josh mora <josh0036@yahoo.com>
2023-02-25 21:27:22 +0800

    Create CollisionDetection.cs
    void OnCollisionEnter(Collision col)
        {
            If(col.tag=="Enemy")
               {
                Destroy(col.gameobject)
               }
        }

---
+++ CollisionDetection.cs
@@ -0,0 +1,11 @@
+Using UnityEngine;
+// check the brakets
+Public class CollisionDetection: MonoBehaviour;
+[
+    void OnCollisionEnter(Collision.col)
+    // Other collision method "OnTriggerEnter"
+    // "OnCollisionExit" "OnCollisionStay"
+    //"OnTriggerExit" "OnTriggerStay"
+[
+    Debug.Log(col.gameobject);
+]
